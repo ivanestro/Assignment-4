40878a4075fca9b255aaccb5b115a3da841343ab
Ivan Estropigan <iestropigan@rrc.ca>
2025-03-09 23:36:47 -0500

    Modified Validation 1, 2 and Collect invalid records code to function.

--- pixell_transaction_report.py
+++ pixell_transaction_report.py
@@ -5,39 +5,43 @@ Example:
     $ python pixell_transaction_report.py
 """
 
-__author__ = "ACE Faculty"
-__version__ = "0.11.2024"
+__author__ = "COMP-1327 (265263) Ivan Estropigan"
+__version__ = "2025/03/09"
 
-import csv
-import os
+#Importing csv and os 
+import csv  #csv reads the data file of our directory 
+import os #os is to clear the terminal, track valid transactions, reject records. 
  
-valid_transaction_types = ['deposit', 'withdraw']
-customer_data = {}
-rejected_transactions = []
-transaction_count = 0
-transaction_counter = 0
-total_transaction_amount = 0
-is_valid_record = True
-error_message = ''
+valid_transaction_types = ['deposit', 'withdraw'] # Creating a transaction type either deposit or withdraw
+customer_data = {} # Storing customer data 
+rejected_transactions = [] # reject transaction if it does not align with record transaction.
+transaction_count = 0  # Valid Transaction count.
+transaction_counter = 0 # Transaction counter is valid or invalid.
+total_transaction_amount = 0 # The accumiliative 
+is_valid_record = True # If Current Transaction is valid or not 
+error_message = '' # A string holds error messages relating to invalid transaction.
+transaction_amount = 0 
 
-# Clears the terminal
+
+# Clears the terminal to create a readable console. 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-# Get the directory the script is saved to
+# Get the directory the script is saved to locate our csv file within our directory folder. 
 SCRIPT_DIRECTORY = os.path.dirname(__file__)
 
-# The name of the data file
+# The name of the data file in our directory.
 DATA_FILENAME = "bank_data.csv"
 
-# The absolute path to the data file
+# The absolute path to the data file bank_data.csv
 DATA_FILE_PATH = f"{SCRIPT_DIRECTORY}/{DATA_FILENAME}"
 
-with open(DATA_FILE_PATH, 'r') as csv_file:
-    reader = csv.reader(csv_file)
+with open(DATA_FILE_PATH, 'r') as csv_file: # locates the bank_data.csv
+    reader = csv.reader(csv_file) # Opens it as read mode.
 
-    # Skip heading line
+    # Skip heading line examples Customer ID, Transaction Type, Amount.
     next(reader)
 
+    # Read each transaction for each row.
     for transaction in reader:
         # Reset valid record and error message for each iteration
         is_valid_record = True
@@ -50,10 +54,22 @@ with open(DATA_FILE_PATH, 'r') as csv_file:
         transaction_type = transaction[1]
 
         ### VALIDATION 1 ###
+        if transaction_type not in valid_transaction_types: #Checking if transaction type is not in valid transaction.
+            is_valid_record = False # If it is not in valid record set valid record to false 
+            error_message = f"Not a valid transaction type {transaction_type}" # Error message saying "This valid record is invalid."
 
         ### VALIDATION 2 ###
         # Gets the transaction amount from the third column
-        transaction_amount = float(transaction[2])
+        try:
+            transaction_amount = float(transaction[2])
+        except ValueError:
+            is_valid_record = False
+            error_message = f"{transaction[2]} Not a valid transaction type."
+        
+        if transaction_amount <= 0: 
+            is_valid_record = False 
+            error_message = f"Error Message Validation 2"
+
 
         if is_valid_record:
             # Initialize the customer's account balance if it doesn't 
@@ -77,7 +93,13 @@ with open(DATA_FILE_PATH, 'r') as csv_file:
                 )
         
         ### COLLECT INVALID RECORDS ###
-        
+        else: 
+            error_message 
+            transaction_error = transaction,error_message
+            rejected_transactions.append(transaction_error)
+
+
+
 report_title = "PiXELL River Transaction Report"
 print(report_title)
 print('=' * len(report_title))
@@ -94,10 +116,11 @@ for customer_id, data in customer_data.items():
     for rejected_transaction in data['transactions']:
         amount, type = rejected_transaction
         print(f"{type.capitalize():>16}:{amount:>12}")
-
-average_transaction_amount = total_transaction_amount / transaction_counter
-print(f"AVERAGE TRANSACTION AMOUNT: {average_transaction_amount}")
-
+try:
+    average_transaction_amount = total_transaction_amount / transaction_counter 
+    print(f"AVERAGE TRANSACTION AMOUNT: {average_transaction_amount}")
+except ZeroDivisionError as exception: 
+    print("COLLECT INVALID RECORDS") 
 rejected_report_title = "REJECTED RECORDS"
 print(rejected_report_title)
 print('=' * len(rejected_report_title))
